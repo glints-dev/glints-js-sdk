f9abdb1e8dc0e43a80c4bdb99c46ee2b4b21cb16
hermawan22 <hermawan.remote@gmail.com>
2018-08-16 14:08:28 +0800

    added test, coverage, travis and fix babelrc

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,17 @@
-const { BABEL_ENV, NODE_ENV } = process.env;
+const {BABEL_ENV, NODE_ENV} = process.env;
 
 const cjs = BABEL_ENV === "cjs" || NODE_ENV === "test";
 
 module.exports = {
-  presets: [
-    ["preset-env", { loose: true, modules: false }],
+  presets: ["env"],
+  plugins: [
     "transform-object-rest-spread",
-    "transform-class-properties"
-  ],
-  plugins: [cjs && "transform-es2015-modules-commonjs"].filter(Boolean)
+    "transform-class-properties",
+    cjs && "transform-es2015-modules-commonjs"
+  ].filter(Boolean),
+  env: {
+    test: {
+      plugins: ["istanbul"]
+    }
+  }
 };
---
+++ test/index.spec.js
@@ -0,0 +1,4 @@
+import Gatsby from "../src/index";
+import {expect} from "chai";
+
+describe("Gatbsy JS SDK", function() {});
